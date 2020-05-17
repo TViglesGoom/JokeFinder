5f34eb577d70cf1db525fc48fa819d38a5ec68fb
Vlad Huz <tviglesgoom@gmail.com>
2020-05-17 18:32:24 +0300

    Updates

---
+++ precache-manifest.0f12f1454d503326910b582cdbaecdc9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86eaaa36e07ecc3ae442bd01983f0a1b",
+    "url": "/JokeFinder/index.html"
+  },
+  {
+    "revision": "0884bce439c24bb264ff",
+    "url": "/JokeFinder/static/css/2.a85e43eb.chunk.css"
+  },
+  {
+    "revision": "4d7a8c730ac3bfb24b8a",
+    "url": "/JokeFinder/static/css/main.e9b6d31c.chunk.css"
+  },
+  {
+    "revision": "0884bce439c24bb264ff",
+    "url": "/JokeFinder/static/js/2.00d626ff.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/JokeFinder/static/js/2.00d626ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d7a8c730ac3bfb24b8a",
+    "url": "/JokeFinder/static/js/main.33aad538.chunk.js"
+  },
+  {
+    "revision": "9c5d8406212024441d79",
+    "url": "/JokeFinder/static/js/runtime-main.731edce8.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/JokeFinder/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/JokeFinder/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/JokeFinder/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/JokeFinder/static/media/notification.c0d3c94c.eot"
+  }
+]);
